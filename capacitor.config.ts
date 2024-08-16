f5a620ce9b2decfef01e4eea59e8258c8cd786bb
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2024-08-15 21:47:26 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'proyecto.geometria',
-  appName: 'proyecto-geometria',
-  webDir: 'www'
-};
-
-export default config;
