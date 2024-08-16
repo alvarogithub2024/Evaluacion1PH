4c86612e98bec7bf825b02a1c08aecf00d19a4cc
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2024-08-15 21:42:27 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'proyecto.geometria',
+  appName: 'proyecto-geometria',
+  webDir: 'www'
+};
+
+export default config;
